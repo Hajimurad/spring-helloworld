47ee03d41bef3f12e91ec6d30d58d280c6baf4d1
Hajimurad <88492690+Hajimurad@users.noreply.github.com>
2021-08-11 14:56:20 +0300

    Update AppConfig.java

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class AppConfig {
  
-    @Bean(name="helloworld")
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
@@ -15,7 +15,7 @@ public class AppConfig {
     @Bean(name = "cat")
     @Scope("prototype")
     public Cat getCat(){
-        Cat cat =new Cat();
+        Cat cat = new Cat();
         cat.setMessage("Mew");
         return cat;
     }
